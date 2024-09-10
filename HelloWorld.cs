627abebd019d39b8176ab3258e15a84f59bba920
Mohaek10 <elkasmimoha@gmail.com>
2024-09-10 22:36:13 +0200

    Principales tipos de datos

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,8 +1,36 @@
 ﻿using System;
-class HelloWorld
+
+namespace CSharpHelloWorld
 {
-    static void Main(string[] args)
+    class HelloWorld
     {
-        Console.WriteLine("Hello, CSharp!");
+        static void Main(string[] args)
+        {
+            //Hola mundo
+            Console.WriteLine("Hello, CSharp!");
+            /*
+            Hola a todos esto es un comentario
+            */
+
+            string myString = "Hola a todos";
+            myString = "Cambio de valor";
+            Console.WriteLine(myString);
+
+            int myInt = 7;
+            myInt = myInt + 33;
+            Console.WriteLine(myInt);
+
+            double myDouble = 30;
+            System.Console.WriteLine(myDouble);
+
+            float myFloat = 4.3f;
+            System.Console.WriteLine(myFloat);
+
+            dynamic myDynamic = 6;
+            myDynamic = "Hola que tal he cambiado este dato";
+            System.Console.WriteLine(myDynamic);
+
+
+        }
     }
 }
